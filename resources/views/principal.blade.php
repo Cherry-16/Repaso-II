db112738ad7ad47698de2840e1c26c11d3ca46bf
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-10-31 02:08:01 -0600

    cambio_idioma
    En este commit se ejecutan los comandos: "php artisan lang:publish", se modifica el documento .env para cambiar del idioma español al inglés y se actualiza con este comando " php artisan lang:update"

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -7,9 +7,9 @@
     <h1 class="display-1">Bienvenido Lector!</h1>
     <div class="mt-4 p-3 border border-secondary rounded mb-5" style="width: 80%;">
         <h2 class="h4">Noticia Literaria</h2>
-        <p>El Día de las Bibliotecas se celebra cada 24 de octubre desde el año 1997. Este día nació como iniciativa de la Asociación Española de Amigos del Libro Infantil y Juvenil con el objetivo de concienciar a la sociedad de la importancia de la lectura y como homenaje y reconocimiento a la labor de los bibliotecarios/as.</p>
-        <p>«El lema de este año, «Por un futuro sostenible», pretende destacar el papel fundamental que desempeñan todas las tipologías de bibliotecas (públicas, escolares, universitarias, especializadas y nacionales/regionales) en la consecución de los Objetivos de Desarrollo Sostenible de la Agenda 2030.</p>
-        <p>A través de sus colecciones, servicios y actividades con usuarios, las bibliotecas contribuyen a construir una sociedad cada vez mejor informada, más igualitaria, solidaria y justa, plenamente consciente de la necesidad de llevar un modo de vida sostenible como compromiso con las generaciones futuras.»</p>
+        <p>La Feria Internacional del Libro de Guadalajara 2024, que se llevará a cabo del 30 de noviembre al 8 de diciembre, ha anunciado que el invitado de honor será el país de Italia. Este evento, uno de los más importantes en el ámbito literario de habla hispana, reunirá a autores, editores y lectores de todo el mundo.</p>
+        <p>Durante la feria, se llevarán a cabo presentaciones de libros, charlas con escritores destacados y actividades culturales para todas las edades. Además, se espera que la feria cuente con una amplia variedad de editoriales que presentarán sus nuevas publicaciones y títulos clásicos.</p>
+        <p>El lema de este año es "Lecturas que Transforman", lo que refleja el compromiso de la Feria con la promoción de la lectura como una herramienta para el cambio social y el crecimiento personal. Se invitará a los asistentes a explorar nuevas voces y perspectivas en la literatura contemporánea.</p>
     </div>
 </div>
 @endsection
